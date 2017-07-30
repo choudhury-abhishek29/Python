0f28eaccda8cf84addf8118b0119c22bd0f02dc8
choudhury-abhishek29 <choudhury.abhishek29@gmail.com>
2017-07-30 02:09:32 -0700

    First project on Django framework
    Basic inventory management system to track available items

---
+++ Django/Inventory/Inventory/admin.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from .models import Item
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['title','description','amount']
+
+admin.site.register(Item, ItemAdmin)
